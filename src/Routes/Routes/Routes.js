d3bb9220d65085336f40b3c30db77ae5743b95c8
kamrul-7 <kamrul7.khan@gmail.com>
2022-11-24 02:03:49 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,25 @@
+import Home from "../../pages/Home/Home/Home";
+import Login from "../../pages/login/Login";
+
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+])
+
+export default router;
